f9cff0fbbb1bb7324dc8cee4ac06b5fb64ace73f
山浦功 <ko.foot1103@gmail.com>
2025-10-22 18:18:38 +0900

    feat: add global shortcut to show/hide

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -38,6 +38,48 @@ pub fn run() {
 
             app.manage(AppTray{_tray: tray});
 
+            #[cfg(desktop)]
+            {
+                use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
+                let ctrl_e_shortcut = Shortcut::new(Some(Modifiers::SUPER), Code::KeyE);
+                let toggle_shortcut = ctrl_e_shortcut.clone();
+
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new()
+                        .with_handler(move |app_handle, shortcut, event| {
+                            if shortcut == &toggle_shortcut
+                                && matches!(event.state(), ShortcutState::Pressed)
+                            {
+                                if let Some(window) = app_handle.get_webview_window("main") {
+                                    match window.is_visible() {
+                                        Ok(true) => {
+                                            let _ = window.hide();
+                                        }
+                                        Ok(false) => {
+                                            let _ = window.unminimize();
+                                            let _ = window.show();
+                                            let _ = window.set_focus();
+                                        }
+                                        Err(err) => {
+                                            eprintln!(
+                                                "failed to read window visibility, forcing show: {}",
+                                                err
+                                            );
+                                            let _ = window.unminimize();
+                                            let _ = window.show();
+                                            let _ = window.set_focus();
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                        .build(),
+                )?;
+
+                app.global_shortcut().register(ctrl_e_shortcut)?;
+            }
+
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
